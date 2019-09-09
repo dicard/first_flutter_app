71f058fe7079192ef89fc850d9770814127cab64
Dicard <robin.corona31@gmail.com>
2019-09-09 23:09:35 +0200

    start tutorial part 1 completed

---
+++ lib/RandomWords.dart
@@ -0,0 +1,61 @@
+import 'package:english_words/english_words.dart' as prefix0;
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/widgets.dart';
+
+
+class RandomWords extends StatefulWidget{
+  @override 
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+
+
+
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFront = const TextStyle(fontSize: 18);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      
+      itemBuilder: (BuildContext _context, int i) { 
+        if (i.isOdd) {
+          return Divider(); 
+        }
+
+        final int index = i ~/ 2;
+
+        if(index >= _suggestions.length) {
+          _suggestions.addAll(prefix0.generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text( 
+        pair.asPascalCase,
+        style: _biggerFront,
+      ),
+    );
+  }
+
+
+
+}
